4c0e8969fe8b39161414424b830e0e902a65d711
Rubiromi <hiromi.deyoung@gmail.com>
2014-06-18 16:11:18 -0600

    Added bootstrap, added delete method

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -23,7 +23,25 @@ class PostsController < ApplicationController
 
 
   def edit
+    @post = Post.find(params[:id])
   end
+
+  def update
+    @post = Post.find(params[:id])
+
+    if @post.update_attributes(post_params)
+      redirect_to post_path(@post.id)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    redirect_to posts_path
+  end
+
 
 private
 
