3cb6fba4687987b5a2fbb3913d55239a765401ca
Rubiromi <hiromi.deyoung@gmail.com>
2014-06-18 15:17:15 -0600

    added static controller with about, and home pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  root 'static#home'
+
+  get '/about', to: 'static#about', as: :about
+  # start with Action(get), url looks like('/about'), it goes to('static#about'), it shows on routes as(about)
+
   resources :posts
 
 end
