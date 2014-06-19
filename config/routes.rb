1dbadb8fbeda2aa014598c2024244a422697ea53
Rubiromi <hiromi.deyoung@gmail.com>
2014-06-19 09:04:35 -0600

    adjusted designs

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
+
   root 'static#home'
 
   get '/about', to: 'static#about', as: :about
   # start with Action(get), url looks like('/about'), it goes to('static#about'), it shows on routes as(about)
 
-  resources :posts
+  resources :posts do
+    resources :comments
+  end
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -23,3 +23,21 @@ title:        'My third post',
 author:       'Hiromi De Young',
 description:  'This is my third post. Enjoy my blog!'
 )
+
+Comment.create(
+author: 'Minami',
+message: 'I am singing tonight!',
+post_id: 1
+)
+
+Comment.create(
+author: 'Machida',
+message: 'Yeah, it is Obama\'s fault',
+post_id: 2
+)
+
+Comment.create(
+author: 'Jessie',
+message: 'Yeah, I think so too',
+post_id: 3
+)
